e6fbe00b59076eeb5b4b21c9c52bf0e04e3ed282
Gabriel Angelo <73532594+angelog@users.noreply.github.com>
2021-06-08 18:25:13 -0300

    arquivo sem uso

--- projetointegrador/py.py
+++
@@ -1,9 +0,0 @@
-
-where = ['candidato.nivelEscolaridade = "medio completo"', 'candidato.pcdCandidato = 2']
-query = "select * from candidato"
-
-for i in range(len(where) != 0):
-    separador = " and "
-    newWhere = separador.join(where)
-    query = query + " where " + newWhere
-    print(query)
